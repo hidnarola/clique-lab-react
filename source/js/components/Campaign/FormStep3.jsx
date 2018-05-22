bae485891385dd3cb8e86720deccfb6019982f27
NAROLA\vparth <pav@narola.email>
2018-05-22 12:31:20 +0530

    new changes

--- source/js/components/Campaign/FormStep3.jsx
+++ source/js/components/Campaign/FormStep3.jsx
@@ -1,98 +1,148 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import FormCampaignRight from './FormCampaignRight';
-import {CommonCompo} from './CommonCompo';
+import { CommonCompo } from './CommonCompo';
 import { Field, reduxForm } from 'redux-form';
 import DatePicker from 'react-datepicker';
 import validator from 'validator';
 import moment from 'moment';
-import {renderFieldCampaign,renderFieldDatePicker,SelectField_ReactSelect,SelectField_ReactSelectMulti} 
-        from '../../components/Forms/RenderFormComponent/EveryComponent';
+import cx from 'classnames';
+import { renderFieldCampaign, renderFieldDatePicker, SelectField_ReactSelect, SelectField_ReactSelectMulti, renderPlaceAutoComplete }
+    from '../../components/Forms/RenderFormComponent/EveryComponent';
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
 
 const validate = values => {
 
     const errors = {};
 
-    if (!values.public_or_private || values.public_or_private.value==="") {
+    if (!values.public_or_private || values.public_or_private.value === "") {
         errors.public_or_private = 'This field is required';
     }
 
-    if(!values.media_format || values.media_format.value===""){
+    if (!values.media_format || values.media_format.value === "") {
         errors.media_format = 'This field is required';
     }
- 
-    if(!values.location || (values.location!==undefined && values.location.trim()=="")){
+
+    if (!values.location || (values.location !== undefined && values.location.trim() == "")) {
         errors.location = 'This field is required';
     }
 
-    if(!values.how_much){
+    if (!values.how_much) {
         errors.how_much = 'This field is required';
-    }else if(!validator.isFloat(values.how_much)){
+    } else if (!validator.isFloat(values.how_much)) {
         errors.how_much = 'Must be a number';
     }
-    else if(!validator.matches(values.how_much,/^[0-9]/))
-    {
+    else if (!validator.matches(values.how_much, /^[0-9]/)) {
         errors.how_much = 'Must be a positive value';
     }
 
-    if(!values.currency || values.currency.value===""){
+    if (!values.currency || values.currency.value === "") {
         errors.currency = 'This field is required';
     }
-
+    console.log(errors);
     return errors;
 };
 
-class FormStep3 extends Component{
-    constructor(props){
+
+
+class FormStep3 extends Component {
+    constructor(props) {
         super(props);
+        this.state = { address: '' }
     }
 
-    render(){
-        const { handleSubmit,previousPage } = this.props;
-        return(
+    handleChange = (address) => {
+        console.log('address ->',address,'--');
+        this.setState({ address })
+    }
+
+    handleSelect = (address) => {
+        geocodeByAddress(address)
+            .then(results => getLatLng(results[0]))
+            .then(latLng => console.log('Success', latLng))
+            .catch(error => console.error('Error', error))
+    }
+
+    render() {
+        const { handleSubmit, previousPage } = this.props;
+        let { address } = this.state;
+        return (
             <form onSubmit={handleSubmit}>
                 <div className="right-box create-campaign d-flex">
                     <div className="create-campaign-l d-flex">
                         <CommonCompo currentPage="3" changePage={(i) => this.props.changePage(i)} />
                         <div className="step-content d-flex">
                             <h2>Step 3</h2>
-                            <Field        
+                            <Field
                                 wrapperClass="select-wrap"
-                                name="public_or_private"       
+                                name="public_or_private"
                                 label="Public or Invite only"
                                 labelClass="control-label"
                                 placeholder="Public or Invite only"
                                 component={SelectField_ReactSelect}
                                 options={[
                                     { value: '', label: 'Select Industry' },
-                                    { value: 'public' , label :"Public"},
-                                    { value: 'invite' , label :"Private"}                                    
+                                    { value: 'public', label: "Public" },
+                                    { value: 'invite', label: "Private" }
                                 ]}
-                                isRequired="true"                                             
+                                isRequired="true"
                             />
 
-                            <Field        
+                            <Field
                                 wrapperClass="select-wrap"
-                                name="media_format"       
+                                name="media_format"
                                 label="Media Format"
                                 labelClass="control-label"
                                 placeholder="Media Format"
                                 component={SelectField_ReactSelect}
                                 options={[
                                     { value: '', label: 'Select Media Format' },
-                                    { value: 'photograph' , label :"Photograph"},
-                                    { value: 'video' , label :"Video"}                                    
-                                ]} 
-                                isRequired="true"                                            
+                                    { value: 'photograph', label: "Photograph" },
+                                    { value: 'video', label: "Video" }
+                                ]}
+                                isRequired="true"
                             />
-                            
-                            <Field
+
+                            {/* <Field
                                 name="location"
                                 type="text"
                                 label="Location"
                                 component={renderFieldCampaign}
                                 placeholder="Write Location"
                                 isRequired="true"
-                            />
+                            /> */}
+
+                            <PlacesAutocomplete
+                                value={this.state.address}
+                                onChange={this.handleChange}
+                                onSelect={this.handleSelect}
+                            >
+                                {({ getInputProps, suggestions, getSuggestionItemProps }) => (
+
+                                    <div>
+                                        {/* <label>Location <span className="error-div">*</span></label>
+                                        <input
+                                            {...getInputProps({
+                                                placeholder: 'Write location',
+                                                className: 'location-search-input',
+                                                name: 'location',
+                                            })}
+                                        /> */}
+                                        <Field
+                                            getInputProps={getInputProps}
+                                            suggestions={suggestions}
+                                            getSuggestionItemProps={getSuggestionItemProps}
+                                            name="location"
+                                            type="text"
+                                            label="Location"
+                                            component={renderPlaceAutoComplete}
+                                            placeholder='Write location'
+                                            className='location-search-input'
+                                            isRequired="true"
+                                            addressValue={address}
+                                        />
+                                    </div>
+                                )}
+                            </PlacesAutocomplete>
 
                             <Field
                                 name="how_much"
@@ -103,29 +153,29 @@ class FormStep3 extends Component{
                                 isRequired="true"
                             />
 
-                            <Field        
+                            <Field
                                 wrapperClass="select-wrap"
-                                name="currency"       
+                                name="currency"
                                 label="How much to pay ( Currency )"
                                 labelClass="control-label"
                                 placeholder="How much to pay ( Currency )"
                                 component={SelectField_ReactSelect}
                                 options={[
                                     { value: '', label: 'Select Currency' },
-                                    { value: 'dollar' , label :"Dollar"}                                                                        
+                                    { value: 'dollar', label: "Dollar" }
                                 ]}
                                 isRequired="true"
-                                                                            
+
                             />
 
                             <div className="submit-btn d-flex">
-                                <button type="button" onClick={previousPage} 
-                                        className="round-btn prev-btn">Previous</button>
+                                <button type="button" onClick={previousPage}
+                                    className="round-btn prev-btn">Previous</button>
                                 <button type="submit" className="round-btn next-btn">Next</button>
                             </div>
                         </div>
                     </div>
-                    <FormCampaignRight/>
+                    <FormCampaignRight />
                 </div>
             </form>
         );
