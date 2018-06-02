744ab148a8e295844228d40a1d01de8cec736fe6
vpa <vpa@narola.email>
2018-06-02 12:10:07 +0530

    new changes

--- source/js/constants/img_path.js
+++ source/js/constants/img_path.js
@@ -4,7 +4,7 @@ export const publicPath = '/';
 
 export const imgRoutes = {
     GROUP_IMG_PATH: `${SERVER_BASE_URL}uploads/group/`,
-    USER_IMG_PATH: `${SERVER_BASE_URL}uploads/users/`,
+    USER_IMG_PATH: `${SERVER_BASE_URL}uploads/users/300X200/`,
     PROMOTER_IMG_PATH: `${SERVER_BASE_URL}uploads/promoter/80X80/`,
     CAMPAIGN_IMG_PATH: `${SERVER_BASE_URL}uploads/campaign/512X384/`,
     CAMPAIGN_INSPIRED_IMG_PATH: `${SERVER_BASE_URL}uploads/inspired_submission/`,
