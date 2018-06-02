10a481fb19eeaeb4927bf802be812fca09a93036
NAROLA\vparth <pav@narola.email>
2018-06-02 12:41:53 +0530

    new chnages

--- source/js/components/Analytics/Stats.jsx
+++ source/js/components/Analytics/Stats.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import graph from 'img/site/graph.jpg';
 // import { BarChart } from 'react-d3-components';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'Recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem, UncontrolledDropdown } from 'reactstrap';
 
